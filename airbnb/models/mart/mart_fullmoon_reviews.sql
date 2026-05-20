0ebd25a52bc315c78f342d21e02b0ef9fe3f4f61
Naga-Lokesh <vnagalokesh9796@gmail.com>
2026-05-20 19:11:29 +0530

    till seed

---
+++ airbnb/models/mart/mart_fullmoon_reviews.sql
@@ -0,0 +1,20 @@
+{{ 
+    config(
+        materialized = 'table'
+    )
+}}
+
+WITH fct_reviews AS (
+    SELECT * FROM {{ ref('fct_reviews') }}
+),
+full_moon_dates AS (
+    SELECT * FROM {{ ref('seed_full_moon_dates') }}
+)
+SELECT R.*,
+CASE 
+    WHEN FM.full_moon_date IS NULL THEN 'not full moon'
+    ELSE 'full moon'
+END AS is_full_moon
+FROM FCT_REVIEWS R
+LEFT JOIN full_moon_dates FM
+ON (TO_DATE(R.REVIEW_DATE) = DATEADD(DAY,1,FM.full_moon_date))
